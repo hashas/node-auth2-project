0d837aa3bcd54634c384eb1f175a026707d51273
hh <hh@hhmbp15.local>
2020-05-27 02:07:25 +0100

    initialized project and tested "Welcome" http request successfully

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const helmet = require("helmet")
+// missing cors
+
+
+const server = express()
+const port = process.env.port || 4000 // uppercase "port"?
+
+server.use(helmet())
+server.use(express.json())
+
+server.get("/", (req, res, next) => {
+	res.json({
+		message: "Welcome to our API",
+	})
+})
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Running at http://localhost:${port}`)
+})
